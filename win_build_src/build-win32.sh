06564b58dd4293356c040efcdae20512affd59be
yuxiaofei <yuxiaofei@gamed9.com>
2016-01-06 10:33:07 +0800

    win32&win64 build

--- win_build_src/build-win32.sh
+++ win_build_src/build-win32.sh
@@ -16,23 +16,21 @@ mingw32-make clean
 mingw32-make BUILDMODE=static CC="gcc -m32"
 cp src/libluajit.a ../window/x86/libluajit.a
 
-cd ../pbc/
-make clean
-make BUILDMODE=static CC="gcc -m32"
-cp build/libpbc.a ../window/x86/libpbc.a
+# cd ../pbc/
+# make clean
+# make BUILDMODE=static CC="gcc -m32"
+# cp build/libpbc.a ../window/x86/libpbc.a
 
-cd ../cjson/
-make clean
-make BUILDMODE=static CC="gcc -m32"
-cp build/libcjson.a ../window/x86/libcjson.a
+# cd ../cjson/
+# make clean
+# make BUILDMODE=static CC="gcc -m32"
+# cp build/libcjson.a ../window/x86/libcjson.a
 
 cd ..
 
 gcc lua_wrap.c \
-	pb_win.c \
+	../struct-0.2/struct.c \
 	lpeg.c \
-	sproto.c \
-	lsproto.c \
 	luasocket/src/luasocket.c \
 	luasocket/src/timeout.c \
 	luasocket/src/buffer.c \
@@ -45,16 +43,11 @@ gcc lua_wrap.c \
 	luasocket/src/except.c \
 	luasocket/src/select.c \
 	luasocket/src/wsocket.c \
-	pbc/binding/lua/pbc-lua.c \
-	cjson/lua_cjson.c \
 	-o Plugins/x86/ulua.dll -m32 -shared \
 	-I./ \
 	-Iluajit/src \
-	-Ipbc \
-	-Icjson \
 	-Iluasocket/src \
+	-I../struct-0.2 \
 	-Wl,--whole-archive \
 	window/x86/libluajit.a \
-	window/x86/libpbc.a \
-	window/x86/libcjson.a \
 	-Wl,--no-whole-archive -lwsock32 -static-libgcc -static-libstdc++
--- win_build_src/build-win64.sh
+++ win_build_src/build-win64.sh
@@ -16,23 +16,21 @@ mingw32-make clean
 mingw32-make BUILDMODE=static CC="gcc -m64"
 cp src/libluajit.a ../window/x86_64/libluajit.a
 
-cd ../pbc/
-make clean
-make BUILDMODE=static CC="gcc -m64"
-cp build/libpbc.a ../window/x86_64/libpbc.a
+# cd ../pbc/
+# make clean
+# make BUILDMODE=static CC="gcc -m64"
+# cp build/libpbc.a ../window/x86_64/libpbc.a
 
-cd ../cjson/
-make clean
-make BUILDMODE=static CC="gcc -m64"
-cp build/libcjson.a ../window/x86_64/libcjson.a
+# cd ../cjson/
+# make clean
+# make BUILDMODE=static CC="gcc -m64"
+# cp build/libcjson.a ../window/x86_64/libcjson.a
 
 cd ..
 
 gcc lua_wrap.c \
-	pb_win.c \
+	../struct-0.2/struct.c \
 	lpeg.c \
-	sproto.c \
-	lsproto.c \
 	luasocket/src/luasocket.c \
 	luasocket/src/timeout.c \
 	luasocket/src/buffer.c \
@@ -45,16 +43,11 @@ gcc lua_wrap.c \
 	luasocket/src/except.c \
 	luasocket/src/select.c \
 	luasocket/src/wsocket.c \
-	pbc/binding/lua/pbc-lua.c \
-	cjson/lua_cjson.c \
 	-o Plugins/x86_64/ulua.dll -m64 -shared \
 	-I./ \
 	-Iluajit/src \
-	-Ipbc \
-	-Icjson \
 	-Iluasocket/src \
+	-I../struct-0.2 \
 	-Wl,--whole-archive \
 	window/x86_64/libluajit.a \
-	window/x86_64/libpbc.a \
-	window/x86_64/libcjson.a \
 	-Wl,--no-whole-archive -lwsock32 -static-libgcc -static-libstdc++
